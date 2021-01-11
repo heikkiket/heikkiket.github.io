e1b9f7823156ec8b85d2a3d73a24508b91f7cc1a
Heikki Ketoharju <heikki.ketoharju@fns.fi>
2021-01-11 20:35:36 +0200

    Added links to resume

--- pages/about.vue
+++ pages/about.vue
@@ -3,5 +3,7 @@
         <h2>Hieman minusta</h2>
         <p>Synnyin vuonna 1989 Joensuussa, ja loppu on historiaa.</p>
         <p>Laitan tähän kohtapuoliin oleellisimmat infot itsestäni.</p>
+        <p><a href="/cv/resume.html">Voit myös lukea ansioluetteloni.</a></p>
+        <p><a href="/cv/resume.pdf">[Lataa PDF]</a></p>
     </div>
 </template>
