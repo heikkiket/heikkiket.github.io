0b903801958cb9a87c91ece5ec4d704c41af1561
Heikki Ketoharju <heikki.ketoharju@fns.fi>
2021-01-11 21:12:11 +0200

    Small fix

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,6 @@
     <div>
         <h2>Hieman minusta</h2>
         <p>Synnyin vuonna 1989 Joensuussa, ja loppu on historiaa.</p>
-        <p>Laitan tähän kohtapuoliin oleellisimmat infot itsestäni.</p>
         <p><a href="/cv/resume.html">Voit myös lukea ansioluetteloni.</a></p>
         <p><a href="/cv/resume.pdf">[Lataa PDF]</a></p>
     </div>
