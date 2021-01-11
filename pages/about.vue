426a84b1e2667dec1fb3b0e969413ff02ef7d015
Heikki Ketoharju <heikki.ketoharju@fns.fi>
2021-01-11 22:15:30 +0200

    Small fixes to static pages

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Hieman minusta</h2>
         <p>Synnyin vuonna 1989 Joensuussa, ja loppu on historiaa.</p>
-        <p><a href="/cv/resume.html">Voit myös lukea ansioluetteloni.</a></p>
-        <p><a href="/cv/resume.pdf">[Lataa PDF]</a></p>
+        <p><a href="/cv/resume.html">Löydät tämän linkin takaa ansioluetteloni.</a></p>
+        <p><a href="/cv/resume.pdf">[Lataa PDF-versio ansioluettelosta]</a></p>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <h2>Terve vaan!</h2>
-  <p>Olen pian valmistuva insinööriopiskelija Metropoliasta, ja haluan kirjoittaa laadukasta koodia.</p>
+        <p>Olen pian valmistuva insinööriopiskelija Metropoliasta.</p>
 
   <p>Näillä sivuilla esittelen ohjelmointitöitäni. Ulkoasustakin ehkä huomaa, että olen backend-painottunut ohjelmoija, mutta kyllä fronttikin taittuu.</p>
 
-        <p>Itse asiassa tämä sivusto on ysäri-ulkonäöstään huolimatta 2020-vuosimallin teknologiaa: Pellin alla nimittäin puksuttaa Nuxt.js:llä ja Vuella kokoon kasattu pieni staattinen web-sovellus.</p>
+        <p>Itse asiassa tämä sivusto on ysäri-ulkonäöstään huolimatta 2021-vuosimallin teknologiaa: Pellin alla nimittäin puksuttaa Nuxt.js:llä ja Vuella kokoon kasattu pieni staattinen web-sovellus.</p>
 
   <p>Mielestäni hyvä tietokonejärjestelmä on nopea, luotettava ja yksinkertainen. Sitä voivat käyttää kaikenlaiset ihmiset, itse valitsemallaan tavalla.</p>
     </div>
