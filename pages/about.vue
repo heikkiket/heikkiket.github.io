b06a400beb3553594f3f21bd8ce2dd8a374babd6
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-10-26 20:08:59 +0300

    Earlier changes to portfolio, nine months ago

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <h2>Hieman minusta</h2>
-        <p>Synnyin vuonna 1989 Joensuussa, ja loppu on historiaa.</p>
+        <p>Olen Heikki Ketoharju, neljännen vuoden insinööriopiskelija Metropolia ammattikorkeakoulusta. Ikää on 31 vuotta.</p>
+        <p>Olen myös pitkän linjan tietokoneharrastaja ja Linux-intoilija. Siirryin kokopäiväisesti Linux-käyttäjäksi vuonna 2006, ja siitä pitäen olen säätänyt ja värkännyt järjestelmän parissa. Olen myös osallistunut suomenkielisen käyttäjäyhteisön toimintaan.</p>
+        <p>Tällä hetkellä työskentelen Finnish Net Solutions -nimisessä nopeasti kasvavassa IT-palveluyrityksessä. Työtehtävät sisältävät full stack -kehitystä Vuella ja PHP:llä. Puuhailen hieman myös Linux-hommien kanssa, kun säädän kehitysympäristön kontteja.</p>
+        <p>Osallistun ehtiessäni avoimen lähdekoodin ohjelmistokehitykseen. Viimeisen parin vuoden aikana olen tehnyt bugikorjauksia Meld-ohjelmaan, joka on Pythonilla ja GTK-käkyttöliittymäkirjastolla kirjoitettu työpöytäsovellus Linuxille, Macille ja Windowsille.</p>
+        <p>Koulussa olen opiskellut pääaineenani ohjelmistotuotantoa. Aika suuri osa opinnoista on kulunut oliopohjaista Javaa naputellessa tai web-pohjaisia sovelluksia kehittäessä. Olen käynyt tilastomatematiikan ja datanlouhinnan alkeiskurssit sekä algoritmit ja tietorakenteet -kurssin osana pakollisia opintoja.</p>
+        <p>Valinnaisina minulla on ollut C- ja C++-ohjelmointia parikin kurssia, sekä Funktionaalista ohjelmointia Scalalla. Keväällä on tulossa vielä kurssi modernista web-kehityksestä. </p>
         <p><a href="/cv/resume.html">Löydät tämän linkin takaa ansioluetteloni.</a></p>
         <p><a href="/cv/resume.pdf">[Lataa PDF-versio ansioluettelosta]</a></p>
     </div>
